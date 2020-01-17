2171b4eee82d9833a5283763aca51ba05560f90d
Nima Shoghi <nimashoghi@gmail.com>
2020-01-17 15:26:20 -0500

    Initial commit

---
+++ ui.js
@@ -0,0 +1,21 @@
+const reactRules = {
+    "react/jsx-indent": "off",
+    "react/jsx-indent-props": "off",
+    "react/jsx-closing-bracket-location": "off",
+    "react/prop-types": "off",
+    "react/jsx-curly-newline": "off",
+};
+
+const importRules = {
+    "import/prefer-default-export": "off",
+    "import/no-unresolved": "off",
+    "import/no-extraneous-dependencies": "off",
+};
+
+module.exports = {
+    extends: ["eslint-config-react-native-wcandillon"],
+    rules: {
+        ...reactRules,
+        ...importRules,
+    },
+};
